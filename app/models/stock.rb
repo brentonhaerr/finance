005a91a89717f0cdaeeecd6b78eaa136050aa694
Brenton Haerr <brenton.haerr@gmail.com>
2020-05-31 11:08:01 -0700

    L257 added stock new_lookup method and API keys to credentials.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+  
+  #self defines a class method rather than an instance method
+  def self.new_lookup(ticker_symbol)
+      client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex[:sandbox_publishable_token],
+                                    endpoint: 'https://sandbox.iexapis.com/v1')
+      client.price(ticker_symbol)
+  end
+
 end
